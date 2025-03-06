3d3d6d2828345b4465f24c1b34728f1d50afcee0
Nicolas Bonamy <nicolas@bonamy.fr>
2025-03-06 02:16:55 -0600

    test

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
 try {
   module.exports = require('node-gyp-build')(__dirname)
+  console.log('autolib loaded native module', Object.keys(module.exports))
 } catch (err) {
-  console.error('Failed to load native module:', err)
+  console.error('autolib failed to load native module:', err)
   module.exports = {
     sendCtrlKey: function() {
       console.warn('autolib: SendCtrlKey mock')
